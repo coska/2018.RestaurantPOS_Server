c5987b25b9d46b81de22839cbb438f8736d1389a
shindn89 <shindn89@gmail.com>
2018-06-28 19:30:50 -0400

    modify sampledata

--- src/main/resources/sql/sampleData.sql
+++ src/main/resources/sql/sampleData.sql
@@ -13,15 +13,15 @@ INSERT INTO employee (userId, userName, createdAt, updatedAt) VALUES('124', 'nam
 
 INSERT INTO employee (userId, userName, createdAt, updatedAt) VALUES('125', 'name3', CURTIME(), CURTIME());
 
-INSERT INTO category (categoryId, categoryName, needToCook ,createdAt, updatedAt) VALUES(1, 'Beer1', False, CURTIME(), CURTIME());
+INSERT INTO category (categoryId, name, needToCook ,createdAt, updatedAt) VALUES(1, 'Beer1', False, CURTIME(), CURTIME());
 
-INSERT INTO category (categoryId, categoryName, needToCook ,createdAt, updatedAt) VALUES(2, 'food1', True, CURTIME(), CURTIME());
+INSERT INTO category (categoryId, name, needToCook ,createdAt, updatedAt) VALUES(2, 'food1', True, CURTIME(), CURTIME());
 
-INSERT INTO category (categoryId, categoryName, needToCook ,createdAt, updatedAt) VALUES(3, 'food2', True, CURTIME(), CURTIME());
+INSERT INTO category (categoryId, name, needToCook ,createdAt, updatedAt) VALUES(3, 'food2', True, CURTIME(), CURTIME());
 
-INSERT INTO category (categoryId, categoryName, needToCook ,createdAt, updatedAt) VALUES(4, 'food3', True, CURTIME(), CURTIME());
+INSERT INTO category (categoryId, name, needToCook ,createdAt, updatedAt) VALUES(4, 'food3', True, CURTIME(), CURTIME());
 
-INSERT INTO category (categoryId, categoryName, needToCook ,createdAt, updatedAt) VALUES(5, 'food4', True, CURTIME(), CURTIME());
+INSERT INTO category (categoryId, name, needToCook ,createdAt, updatedAt) VALUES(5, 'food4', True, CURTIME(), CURTIME());
 
 INSERT INTO product (productId, name, price, imageFile, categoryId,  createdAt, updatedAt) VALUES ('1', 'what', 100.01, 'test1.jpg' , 4, CURTIME(), CURTIME());
 
@@ -31,23 +31,23 @@ INSERT INTO product (productId, name, price, imageFile, categoryId, createdAt, u
 
 INSERT INTO product (productId, name, price, imageFile, categoryId, createdAt, updatedAt) VALUES ('5', 'what5', 11.71, 'test4.jpg' , 5, CURTIME(), CURTIME());
 
-INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0851',True,'123',1,CURTIME(), CURTIME());
+INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0851','NEW','123',1,CURTIME(), CURTIME());
 
-INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0852',True,'124',2,CURTIME(), CURTIME());
+INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0852','NEW','124',2,CURTIME(), CURTIME());
 
-INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0853',True,'123',3,CURTIME(), CURTIME());
+INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0853','COOKING','123',3,CURTIME(), CURTIME());
 
-INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0854',True,'123',100,CURTIME(), CURTIME());
+INSERT INTO orders(orderId, status,userId,tableId, createdAT, updatedAt) VALUES('d290f1ee-6c54-4b01-90e6-d701748f0854','COOKING','123',100,CURTIME(), CURTIME());
 
-insert into orderitem(orderitemId,quantity,productId,orderId) values(1,4,'2','d290f1ee-6c54-4b01-90e6-d701748f0852');
-insert into orderitem(orderitemId,quantity,productId,orderId) values(2,10,'3','d290f1ee-6c54-4b01-90e6-d701748f0852');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(300,'2','d290f1ee-6c54-4b01-90e6-d701748f0852');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(200,10,'3','d290f1ee-6c54-4b01-90e6-d701748f0852');
 
-insert into orderitem(orderitemId,quantity,productId,orderId) values(3,10,'3','d290f1ee-6c54-4b01-90e6-d701748f0851');
-insert into orderitem(orderitemId,quantity,productId,orderId) values(4,4,'2','d290f1ee-6c54-4b01-90e6-d701748f0851');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(301,10,'3','d290f1ee-6c54-4b01-90e6-d701748f0851');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(401,4,'2','d290f1ee-6c54-4b01-90e6-d701748f0851');
 
-insert into orderitem(orderitemId,quantity,productId,orderId) values(5,1,'3','d290f1ee-6c54-4b01-90e6-d701748f0853');
-insert into orderitem(orderitemId,quantity,productId,orderId) values(6,2,'2','d290f1ee-6c54-4b01-90e6-d701748f0853');
-insert into orderitem(orderitemId,quantity,productId,orderId) values(7,4,'1','d290f1ee-6c54-4b01-90e6-d701748f0853');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(501,1,'3','d290f1ee-6c54-4b01-90e6-d701748f0853');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(601,2,'2','d290f1ee-6c54-4b01-90e6-d701748f0853');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(701,4,'1','d290f1ee-6c54-4b01-90e6-d701748f0853');
 
 
-insert into orderitem(orderitemId,quantity,productId,orderId) values(8,2,'5','d290f1ee-6c54-4b01-90e6-d701748f0854');
+insert into orderitem(orderitemId,quantity,productId,orderId) values(801,2,'5','d290f1ee-6c54-4b01-90e6-d701748f0854');
